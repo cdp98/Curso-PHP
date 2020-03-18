79117e13ad86403a10f048e8c76ee15fe579e37c
Carlos Daniel <carlos.daniel@devinsights.in>
2020-03-18 10:33:07 -0300

    design

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Página teste</title>
+        <link href="style.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="topo">
+          <div class="logo">Carlos</div>
+          <div class="menu">Daniel</div>  
+        </div>
+        <div class="corpo"></div>
+        <div class="rodape"></div>
+    </body>    
+</html>
