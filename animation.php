48d7904d7f927b06217ce737ede69f7f74a0406f
Carlos Daniel <carlos.daniel@devinsights.in>
2020-03-20 12:27:30 -0300

    Animation

---
+++ animation.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title></title>
+        <meta id="viewport" name="viewport" content="width=device-width, user-scalable=no">
+        <link href="animation.css" rel="stylesheet">
+    </head>
+    <bdy>
+        <div class="exemplo"></div>
+    </body>
+</html>
