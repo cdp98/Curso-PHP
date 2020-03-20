06dce371369d7a467c24dc42e6f940f514464a2a
Carlos Daniel <carlos.daniel@devinsights.in>
2020-03-20 11:07:47 -0300

    creat form

---
+++ formulario.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title></title>
+        <meta id="viewport" name="viewport" content="width=device-width, user-scalable=no">
+        <link href="style2.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="formulario">
+            <form method="POST">
+                Usuário: </br>
+                <input type="email" name="email" placeholder="E-mail" autofocus required autocomplete="off"/> </br> </br>
+                Senha: </br>
+                <input type="password" name="senha" placeholder="Senha" maxlength="8" required/> </br> </br>
+
+                <input type="submit" value="Enviar" /> </br> </br>
+            </form>
+        </div>
+    </body>
+</html>
