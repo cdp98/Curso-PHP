df03030480aa8210ccd375781c66ccf3d1ab152f
Carlos Daniel <carlos.daniel@devinsights.in>
2020-03-20 14:20:34 -0300

    shadow

---
+++ shadows.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Sombras</title>
+        <meta id="viewport" name="viewport" content="width=device-width, user-scalable=no">
+        <link href="shadows.css" rel="stylesheet">
+    </head>
+    <bdy>
+        <div class="exemplo">Aaawwoooobbaaa, Bão ?</div>
+    </body>
+</html>
