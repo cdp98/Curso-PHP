883a2dc2599b94d4699bdd89daf03b297f9914cc
Carlos Daniel <carlos.daniel@devinsights.in>
2020-03-20 11:55:36 -0300

    gradient

---
+++ gradient.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title></title>
+        <meta id="viewport" name="viewport" content="width=device-width, user-scalable=no">
+        <link href="stylegradient.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="exemplo"></div>
+    </body>
+</html>
