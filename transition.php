f540bbeb96792b9bf654887b9d11a08d4f155689
Carlos Daniel <carlos.daniel@devinsights.in>
2020-03-20 14:39:12 -0300

    Transition

---
+++ transition.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Transição</title>
+        <meta id="viewport" name="viewport" content="width=device-width, user-scalable=no">
+        <link href="transition.css" rel="stylesheet">
+    </head>
+    <bdy>
+        <div class="exemplo"></div>
+
+        <a href:>google</a>
+    </body>
+</html>
