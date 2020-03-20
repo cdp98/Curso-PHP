a444bbbfa11bbc0bddd474e64bcc4e213f11d507
Carlos Daniel <carlos.daniel@devinsights.in>
2020-03-20 11:25:48 -0300

    boreder rounded

---
+++ border.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title></title>
+        <meta id="viewport" name="viewport" content="width=device-width, user-scalable=no">
+        <link href="style3.css" rel="stylesheet">
+    </head>
+    <body>
+        <div id="teste">
+        </div>
+        <div id="teste2">
+        </div>
+    </body>
+</html>
