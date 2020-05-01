5b59aac11983e9ae92e757395d70f2b43e0284b3
Carlos Daniel <carlos.daniel@devinsights.in>
2020-04-30 21:32:31 -0300

    Configuring MVC in the devsbook

---
+++ devsbook/src/Config.php
@@ -0,0 +1,15 @@
+<?php
+namespace src;
+
+class Config {
+    const BASE_DIR = '/devsbook/public';
+
+    const DB_DRIVER = 'mysql';
+    const DB_HOST = 'mysql';
+    const DB_DATABASE = 'devsbook';
+    const DB_USER = 'root';
+    const DB_PASS = 'master';
+
+    const ERROR_CONTROLLER = 'ErrorController';
+    const DEFAULT_ACTION = 'index';
+}
